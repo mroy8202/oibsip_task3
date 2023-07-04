fd4a333617b6d3461dc8561bd988cbdc4ad73bb5
mritunjayRoy <mroy8202@gmail.com>
2023-07-05 04:28:19 +0530

    local storage functionality added

--- script.js
+++ script.js
@@ -2,130 +2,160 @@ const inputContainer = document.querySelector(".input-container");
 const addTaskButton = document.querySelector(".add-button");
 const pendingTaskContainer = document.querySelector("#pending-task-container");
 const completedTaskContainer = document.querySelector("#completed-task-container");
-const informationContainer = document.querySelector("#information-container");
+const reset = document.querySelector(".reset");
 
-// event listener on addTaskButton
-addTaskButton.addEventListener('click', addTask);
-document.addEventListener('keydown', (event) => {
-    if(event.key === 'Enter') {
+// Retrieve tasks from local storage when page loads
+document.addEventListener("DOMContentLoaded", () => {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach((task) => {
+        createTaskElement(task);
+    });
+});
+
+// Event listener on addTaskButton
+addTaskButton.addEventListener("click", addTask);
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
         addTask();
     }
 });
 
-// addTask function
+// Event listener on reset button
+reset.addEventListener("click", resetToDo);
+
+// Add task function
 function addTask() {
-    if(inputContainer.value === "") {
+    if (inputContainer.value === "") {
         console.log("Input field is empty");
-        saveTaskData();
-    }
-
+    } 
+    
     else {
-        // text inside input field
+        // Text inside input field
         const toDoTask = inputContainer.value;
-        // console.log(toDoTask);
 
-        // creating element and adding classes
-        const task = document.createElement('div');
-        task.classList.add("task");
+        // Create task object
+        const task = {
+            content: toDoTask,
+            completed: false,
+        };
 
-        const taskContent = document.createElement('div');
-        taskContent.classList.add("task-content");
+        // Add task to local storage
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
 
-        const roundDivContainer = document.createElement('div');
-        roundDivContainer.classList.add("round-div-container", "pending-round-div-container");
+        // Create task element and append to the appropriate container
+        createTaskElement(task);
 
-        const taskText = document.createElement('p');
-        taskText.classList.add("text", "pending-task-text");
-        taskText.innerText = toDoTask;
-
-        const crossRoundDiv = document.createElement('div');
-        crossRoundDiv.classList.add("cross-round-div");
-
-        const crossButton = document.createElement('p');
-        crossButton.innerText = "x";
-
-        // append the elements
-        pendingTaskContainer.appendChild(task);
-        task.appendChild(taskContent);
-        task.appendChild(crossRoundDiv);
-        taskContent.appendChild(roundDivContainer);
-        taskContent.appendChild(taskText);
-        crossRoundDiv.appendChild(crossButton);
-
-        // empty input field
+        // Empty input field
         inputContainer.value = "";
-
-        // event listeners on pending tasks
-        roundDivContainer.addEventListener('click', switchTask);
-        crossRoundDiv.addEventListener('click', removeFromList);
-
-        saveTaskData();
     }
 }
 
-// switch task function
+// Create task element and append to the appropriate container
+function createTaskElement(task) {
+    const { content, completed } = task;
+
+    // Create elements
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskContent = document.createElement("div");
+    taskContent.classList.add("task-content");
+
+    const roundDivContainer = document.createElement("div");
+    roundDivContainer.classList.add("round-div-container");
+
+    const taskText = document.createElement("p");
+    taskText.classList.add("text");
+    taskText.innerText = content;
+
+    const crossRoundDiv = document.createElement("div");
+    crossRoundDiv.classList.add("cross-round-div");
+
+    const crossButton = document.createElement("p");
+    crossButton.innerText = "x";
+
+    // Append the elements
+    taskElement.appendChild(taskContent);
+    taskElement.appendChild(crossRoundDiv);
+    taskContent.appendChild(roundDivContainer);
+    taskContent.appendChild(taskText);
+    crossRoundDiv.appendChild(crossButton);
+
+    // Add classes based on completion status
+    if (completed) {
+        roundDivContainer.classList.add("completed-round-div-container");
+        taskText.classList.add("completed-text");
+        completedTaskContainer.appendChild(taskElement);
+    } 
+    
+    else {
+        roundDivContainer.classList.add("pending-round-div-container");
+        pendingTaskContainer.appendChild(taskElement);
+    }
+
+    // Event listeners on task elements
+    roundDivContainer.addEventListener("click", switchTask);
+    crossRoundDiv.addEventListener("click", removeFromList);
+}
+
+// Switch task function
 function switchTask() {
-    const task = this.closest('.task');
+    const task = this.closest(".task");
     const roundDivContainer = task.querySelector(".round-div-container");
     const taskText = task.querySelector(".text");
-    
-    // clicked on pending task
-    if(roundDivContainer.classList.contains("pending-round-div-container")) {
-        roundDivContainer.classList.remove('pending-round-div-container');
-        roundDivContainer.classList.add('completed-round-div-container');
-        taskText.classList.add('completed-text');
-        taskText.classList.remove('pending-task-text');
+
+    // Toggle completion status
+    roundDivContainer.classList.toggle("completed-round-div-container");
+    roundDivContainer.classList.toggle("pending-round-div-container");
+    taskText.classList.toggle("completed-text");
+    taskText.classList.toggle("pending-task-text");
+
+    // Move task element to the appropriate container
+    if (roundDivContainer.classList.contains("completed-round-div-container")) {
         completedTaskContainer.appendChild(task);
-        saveTaskData();
-    }
-
-    // clicked on completed task
+    } 
+    
     else {
-        roundDivContainer.classList.remove('completed-round-div-container');
-        roundDivContainer.classList.add('pending-round-div-container');
-        taskText.classList.remove('completed-text');
-        taskText.classList.add('pending-task-text');
         pendingTaskContainer.appendChild(task);
-        roundDivContainer.removeChild(tickMark);
-        saveTaskData();
     }
+
+    // Update completion status in local storage
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    const taskIndex = Array.from(task.parentElement.children).indexOf(task);
+    tasks[taskIndex].completed = !tasks[taskIndex].completed;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// removeFromList function
+// Remove task from list
 function removeFromList() {
-    const task = this.closest('.task');
+    const task = this.closest(".task");
     task.remove();
-    saveTaskData();
+
+    // Remove task from local storage
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    const taskIndex = Array.from(task.parentElement.children).indexOf(task);
+    tasks.splice(taskIndex, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-const reset = document.querySelector(".reset");
-reset.addEventListener('click', resetToDo);
-
+// Reset to-do list
 function resetToDo() {
-    // remove all tasks from pending task container if there are any
-    while(pendingTaskContainer.children.length > 1) {
-        pendingTaskContainer.removeChild(pendingTaskContainer.children[1]);
+    // Remove all tasks from pending task container
+    while (pendingTaskContainer.children.length > 1) {
+    pendingTaskContainer.removeChild(pendingTaskContainer.children[1]);
     }
 
-    // remove all tasks from completed task if there are any
-    while(completedTaskContainer.children.length > 1) {
-        completedTaskContainer.removeChild(completedTaskContainer.children[1]);
+    // Remove all tasks from completed task container
+    while (completedTaskContainer.children.length > 1) {
+    completedTaskContainer.removeChild(completedTaskContainer.children[1]);
     }
 
-    // remove text from input field if any
+    // Remove tasks from local storage
+    localStorage.removeItem("tasks");
+
+    // Empty input field
     inputContainer.value = "";
-    saveTaskData();
 }
-
-
-// save data on local storage
-function saveTaskData() {
-    localStorage.setItem("data", informationContainer.innerHTML);
-}
-
-// show saved data
-// function showTaskData() {
-//     informationContainer.innerHTML = localStorage.getItem("data");
-// }
-
-// showTaskData();
